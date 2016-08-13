cc9aaef12194ef110b11fd23680c65a046351b90
Weigang Geng <gengwg@gmail.com>
2016-08-13 11:18:26 -0700

    conditional probability

---
+++ chap6_probability.py
@@ -0,0 +1,26 @@
+from __future__ import division
+import random
+
+
+## conditional probability
+def random_kid():
+    return random.choice(["boy", "girl"])
+
+both_girls = 0
+older_girl = 0
+either_girl = 0
+
+random.seed(0)
+for _ in range(10000):
+    younger = random_kid()
+    older = random_kid()
+    if older == "girl":
+        older_girl += 1
+    if older == "girl" and younger == "girl":
+        both_girls += 1
+    if older == "girl" or younger == "girl":
+        either_girl += 1
+
+print "P(both | older):", both_girls / older_girl
+print "P(both | older):", both_girls / either_girl
+
